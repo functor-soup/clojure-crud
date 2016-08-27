481302b8705208087a6f83a7ec5f53bb0e6d38b5
luke <nerorevenge@gmail.com>
2016-08-27 00:38:07 -0400

    part 1 complete: basic select all via dsl

--- src/clojure_crud/core.clj
+++ src/clojure_crud/core.clj
@@ -1,4 +1,14 @@
-(ns clojure-crud.core)
+(ns clojure-crud.core
+  (:use [korma.db]
+        [korma.core]))
+
+(def credentials {:user "katsura" :db "gintama" :password "elizebethe"})
+
+(defdb gintama (mysql credentials))
+
+(defentity polls)
+
+(select polls)
 
 (defn foo
   "I don't do a whole lot."
