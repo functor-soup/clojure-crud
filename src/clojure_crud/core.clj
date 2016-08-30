9cd0c6082c4ea239de6be6af324f79eac7078d3d
luke <nerorevenge@gmail.com>
2016-08-29 23:59:44 -0400

    finally a non-web CRUD version

--- src/clojure_crud/core.clj
+++ src/clojure_crud/core.clj
@@ -8,9 +8,26 @@
 
 (defentity polls)
 
-(select polls)
+(defn getAll []
+ (select polls))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn insert_ [{:keys [name  rank]}]
+  (insert polls (values {:name name :rank rank})))
+
+(defn update_ [{:keys [name rank]}]
+  ;; if rank or id is already present, don't bother
+  (if (empty? (select polls (where {:name [= name]})))
+    (insert_ {:name name :rank rank})
+    (update polls
+            (set-fields {:rank rank})
+            (where {:name [= name]}))))
+
+(defn delete_ [name]
+  (if (empty? (select polls (where {:name [= name]})))
+    {:error "Name does not exist in db"}
+    (delete polls
+            (where {:name [= name]}))))
+
+
+
+
